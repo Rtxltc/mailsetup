2042ad65ccf6a038224c3dca6ad41ba128b1e938
Rtxltc <akavanshu@gmail.com>
2026-07-03 14:20:27 +0530

    Add 'ms.soulmatrix.in' to allowed origins

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://frontend-mailsetup.vercel.app",
-        
+        "ms.soulmatrix.in",
         "http://localhost:5173",
     ],
     allow_credentials=True,
